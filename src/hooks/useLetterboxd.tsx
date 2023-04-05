4ddfe2f2ee8370eb1646db18097be509db1b0a4c
Roman <spiderr.roma@gmail.com>
2023-04-06 00:27:01 +0300

    hook re-render fix

--- src/hooks/useLetterboxd.tsx
+++ src/hooks/useLetterboxd.tsx
@@ -1,42 +1,40 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import z from 'zod';
 
 export const letterboxdSchema = z.object({
-	type: z.string(),
-	date: z.object({published: z.number(), watched: z.number()}),
-	film: z.object({
-		title: z.string(),
-		year: z.string(),
-		image: z.object({
-			tiny: z.string(),
-			small: z.string(),
-			medium: z.string(),
-			large: z.string(),
-		}),
-	}),
-	rating: z.object({text: z.string(), score: z.number()}),
-	review: z.string(),
-	spoilers: z.boolean(),
-	isRewatch: z.boolean(),
-	uri: z.string(),
+    type: z.string(),
+    date: z.object({ published: z.number(), watched: z.number() }),
+    film: z.object({
+        title: z.string(),
+        year: z.string(),
+        image: z.object({
+            tiny: z.string(),
+            small: z.string(),
+            medium: z.string(),
+            large: z.string(),
+        }),
+    }),
+    rating: z.object({ text: z.string(), score: z.number() }),
+    review: z.string(),
+    spoilers: z.boolean(),
+    isRewatch: z.boolean(),
+    uri: z.string(),
 });
 
 export type LetterboxdSchema = z.infer<typeof letterboxdSchema>;
 
 export const useLetterboxd = () => {
-	const [items, setItems] = useState<LetterboxdSchema[]>([]);
-	useEffect(() => {
-		async function fetchData() {
-			const response = await fetch('api/letterboxd');
-			const jsonData = await response.json();
-			setItems(jsonData);
-		}
-		fetchData();
-		const interval = setInterval(() => {
-			fetchData();
-		}, 5 * 60 * 1000);
-		return () => clearInterval(interval);
-	}, []); //can't set to items because it will cause an infinite loop
+    const [items, setItems] = useState<LetterboxdSchema[]>([]);
+    const memoizedItems = useMemo(() => items, [items]);
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch('api/letterboxd');
+            const jsonData = await response.json();
+            setItems(jsonData);
+        }
+        fetchData();
+        console.log(memoizedItems);
+    }, []); //can't set to items because it will cause an infinite loop
 
-	return {items};
+    return { items: memoizedItems };
 };
