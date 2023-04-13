6f3c869830dc729205b1a850fa475d540c642385
Roman <spiderr.roma@gmail.com>
2023-04-13 17:50:41 +0300

    yeah

--- src/server/getLetterboxd.ts
+++ src/server/getLetterboxd.ts
@@ -1,7 +1,8 @@
 import { LetterboxdSchema } from '../hooks/useLetterboxd';
 
 type Ip = "localhost" | "127.0.0.0" | "website-git-ssr-zeromero-dev.vercel.app"
-const prodIP = "website-git-ssr-zeromero-dev.vercel.app"
+const prodIP: Ip = "website-git-ssr-zeromero-dev.vercel.app"
+
 export async function getLetterboxd(movieInitial: LetterboxdSchema[]): Promise<LetterboxdSchema> {
 	try {
 		const letterboxd = await fetch(`http://${prodIP}/api/letterboxd`);
